d85126c1c24314e136428b11dc267ec8989ce665
山中彩希 <yamanakasaki@yamanakasakinoMacBook-ea.local>
2021-03-11 10:04:25 +0900

    saving work before angular lesson

--- test-angular-app/src/app/app.component.ts
+++ test-angular-app/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lastname = 'Thompson';
-  bestsensei = 'Nick' + ' ' + this.lastname;
+  name = 'Saki';
 }
